3f2c5b7d35eb61f41fef1276d3b77b57127245a7
ampirzadeh <ampirzadeh@users.noreply.github.com>
2021-08-18 15:28:57 +0000

    deploy: 4febaba02fd6fc0e20dd8a3bb515aa270d8e9ccf

---
+++ precache-manifest.3e86121134d8ed7ae75d9ca8ae1d0950.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c5240c43c8a31bf8053",
+    "url": "/News/css/app.bed6be6c.css"
+  },
+  {
+    "revision": "a4a6b5f780389f5aeb77ad2f9ec388d6",
+    "url": "/News/index.html"
+  },
+  {
+    "revision": "4c5240c43c8a31bf8053",
+    "url": "/News/js/app.45d234b0.js"
+  },
+  {
+    "revision": "da2cdf204decee2a41d4",
+    "url": "/News/js/chunk-vendors.e74ed50a.js"
+  },
+  {
+    "revision": "9541daf96fdd4620866ecba8a821d47b",
+    "url": "/News/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/News/robots.txt"
+  }
+]);
